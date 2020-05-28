6e62a52fc7bce426d55d90fde3cb727345f37133
Kayla Altepeter <kayla@kaylaaltepeter.com>
2020-05-28 16:20:36 -0500

    feat(lessonsService): creating

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LessonsService } from '../shared/services/lessons.service';
 
 @Component({
   selector: 'app-home',
@@ -9,20 +10,12 @@ export class HomeComponent implements OnInit {
   title = `Course Outline`;
   currentLesson = null;
 
-  courseLessons = [
-    { title: 'Hello Angular' },
-    { title: 'Component Fundamentals' },
-    { title: 'Template Driven Forms' },
-    { title: 'Angular Services' },
-    { title: 'Server Communication' },
-    { title: 'Component Driven Architecture' },
-    { title: 'Angular Routing' },
-    { title: 'Unit Testing Fundamentals' },
-  ];
+  courseLessons = null;
 
-  constructor() { }
+  constructor(private lessonsService: LessonsService) { }
 
   ngOnInit(): void {
+    this.courseLessons = this.lessonsService.all();
   }
 
   selectLesson(lesson) {
--- src/app/shared/services/lessons.service.ts
+++ src/app/shared/services/lessons.service.ts
@@ -4,6 +4,36 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class LessonsService {
+  private lessons = [
+    { title: 'Hello Angular' },
+    { title: 'Component Fundamentals' },
+    { title: 'Template Driven Forms' },
+    { title: 'Angular Services' },
+    { title: 'Server Communication' },
+    { title: 'Component Driven Architecture' },
+    { title: 'Angular Routing' },
+    { title: 'Unit Testing Fundamentals' },
+  ];
 
   constructor() { }
+
+  all() {
+    return this.lessons;
+  }
+
+  find(lessonId) {
+    console.log('lessonsService -> find -> lessonId', lessonId);
+  }
+
+  create(lesson) {
+    console.log('lessonsService -> create -> lesson', lesson);
+  }
+
+  update(lesson) {
+    console.log('lessonsService -> update -> lesson', lesson);
+  }
+
+  delete(lessonId) {
+    console.log('lessonsService -> delete -> lessonId', lessonId);
+  }
 }
